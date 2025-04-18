d161044863ef418197e6cdc0407c049db8029ef9
TommyTruong120 <113000317+TommyTruong120@users.noreply.github.com>
2025-04-18 00:49:47 -0700

    Update schedule_priority.c

--- schedule_priority.c
+++ schedule_priority.c
@@ -1,14 +1,50 @@
-/*
- * Priority scheduling
- */
 #include <stdlib.h>
 #include <stdio.h>
-#include <stddef.h>
+#include <string.h>
 
 #include "task.h"
 #include "list.h"
 #include "cpu.h"
-
 /*
- * Your code and design here:
- */
+Add function simply adds the tasks from txt file into a linked list
+
+Priority scheduling function uses linear search approach by first setting two pointers: highest and current
+Initally, first node in the linked list is assigned highiest value. As the the "current" pointer iterates through the nodes,
+it checks its own value with value stored in "highest", if it is current > highest, it will update highest the current value, if not,
+continue iterating. 
+
+*/
+ // empty pointer, points to head of linked list of tasks
+ struct node *task_list = NULL;
+ 
+ // same as add function in fcfs, adds each task to a linked list
+ void add(char *name, int priority, int burst) {
+     Task *newTask = malloc(sizeof(Task));       // Allocate memory for a new Task
+     newTask->name = name;                       
+     newTask->priority = priority;               
+     newTask->burst = burst;                     
+ 
+     insert(&task_list, newTask);               
+ }
+
+// Priority Scheduling
+void schedule() {
+    while (task_list != NULL) {
+        // Both start at the first node
+        struct node *highest = task_list;   
+        struct node *current = task_list;           
+
+        while (current != NULL) { 
+            Task *currTask = current->task; 
+            Task *highTask = highest->task;
+
+            if (currTask->priority < highTask->priority) {
+                highest = current;               
+
+            current = current->next;                  
+        }
+        Task *t = highest->task;
+        run(t, t->burst);                              
+        delete(&task_list, t);                         
+    }
+}
